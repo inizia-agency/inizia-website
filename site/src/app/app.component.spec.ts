58a6555795d757c4f6df5ac337900e1c685dfdef
micaelapiacenza <micaela@inthecode.com.au>
2024-07-02 14:47:40 +1000

    Adjusting routing and nav bar

--- site/src/app/app.component.spec.ts
+++ site/src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'loxi'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('loxi');
+    expect(app.title).toEqual('site');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('loxi app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('site app is running!');
   });
 });
